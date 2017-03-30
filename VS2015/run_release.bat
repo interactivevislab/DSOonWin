2b4a249ab48ddff5c11f5bc6085867ed235149aa
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2017-03-30 12:12:53 +0800

    Starting cleanup for GitHub

---
+++ VS2015/run_release.bat
@@ -0,0 +1,1 @@
+x64\Release\DSOonWin.exe files=dataset\sequence_14\images.zip calib=dataset\sequence_14\camera.txt gamma=dataset\sequence_14\pcalib.txt vignette=dataset\sequence_14\vignette.png preset=0 mode=0
